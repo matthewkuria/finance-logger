18589c93c163f75abd26c442f44170a510342a5a
matthewkuria <mathewkwachira@gmail.com>
2024-04-06 09:57:30 +0300

    Add an event listener

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -74,7 +74,17 @@ const type = document.querySelector('#type') as HTMLSelectElement;
 const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+// add an event listener
+form.addEventListener('submit', (e: Event) =>{
+    e.preventDefault();
+    console.log(
+        type.value,
+        toFrom.value,
+        details.value,
+        amount.value
 
+    )
+})
 
 
 
