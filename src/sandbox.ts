dedaa350615e4d2eec09e1a7147629bb63c5afd1
matthewkuria <mathewkwachira@gmail.com>
2024-04-06 09:54:03 +0300

    finish up input elements

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -71,7 +71,11 @@ console.log(anchor.href);
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 // inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
-const toFrom = document.querySelector('tofrom') as HTMLInputElement;
+const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+
 
 
 console.log(form.childNodes);
